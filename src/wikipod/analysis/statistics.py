5708bc658424f59f97d2694f6061b9e69a423af3
Luca Britten <lucabritten17@icloud.com>
2026-06-13 12:55:54 +0200

    feat: Implemented basic statistic functions, which can be used for scoring articles

--- src/wikipod/analysis/statistics.py
+++ src/wikipod/analysis/statistics.py
@@ -1,17 +1,39 @@
-from models import ArticleMetadata
+from wikipod.analysis.models import ArticleMetadata
+from collections import Counter
 
 def summarize_articles(articles: list[ArticleMetadata]):
-    #     {
-    #     "article_count": 100000,
-    #     "avg_word_count": 420,
-    #     "avg_link_count": 18,
-    #     "max_word_count": 15000
-    # }
-    pass
+    if not articles:
+        return {}
+    
+    article_count = len(articles)
+    
+    word_counts = [a.word_count for a in articles]
+    link_counts = [a.link_count for a in articles]
+    
+    avg_word_count = sum(word_counts) / article_count
+    avg_link_count = sum(link_counts) / article_count
+    
+    max_word_count = max(word_counts)
+    max_links_count = max(link_counts)
+    
+    return {
+        "article_count": article_count,
+        "avg_word_count": avg_word_count,
+        "max_word_count": max_word_count,
+        "avg_link_count": avg_link_count,
+        "max_link_count": max_links_count
+    }
 
-def top_categories(articles, limit=20):
+def top_linked_articles(articles: list[ArticleMetadata], limit=20):
     #     [
     #     ("Physics", 4500),
     #     ("Mathematics", 3900)
     # ]
-    pass
+    counter = Counter()
+    i = 0
+    for article in articles:
+        for link in article.links:
+            counter.update([link])
+        
+    return counter.most_common(limit)
+
